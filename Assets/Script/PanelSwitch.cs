3cf5aac4c94da2bbb5a1dbcef7d78d1a034d4bde
samtlyn <149753737@qq.com>
2015-03-16 21:01:17 +0800

    菜单切换

---
+++ Assets/Script/PanelSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelSwitch : MonoBehaviour {
+
+	// Use this for initialization
+
+	private GameObject panel1 ;
+	private GameObject panel2 ;
+
+	void Start() { 
+		panel1 = GameObject.Find("MainMenu"); 
+		panel2 = GameObject.Find("GameSetting");
+		NGUITools.SetActive(panel2,false);
+	}  
+	void OnClick() { 
+
+		NGUITools.SetActive(panel2,true);
+		NGUITools.SetActive(panel1,false);
+	} 
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
